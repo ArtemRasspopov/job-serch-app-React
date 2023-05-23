bfdaba2b502f4c659e4cdb0c72c8be29d7d8c52f
ARTEM_JAN <Artem199833@gmail.com>
2023-05-23 18:01:18 +0300

    build

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Header } from "./Header/Header";
 import EmptyPage from "../pages/EmptyPage/EmptyPage";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import SearchVacancyPage from "../pages/SearchVacancyPage/SearchVacancyPage";
 import FavoriteVacancyPage from "../pages/FavoritesVacancyPage/FavoritesVacancyPage";
 import VacancyOnePage from "../pages/VacancyOnePage/VacancyOnePage";
@@ -9,7 +9,7 @@ import VacancyOnePage from "../pages/VacancyOnePage/VacancyOnePage";
 function App() {
   return (
     <div className="app">
-      <BrowserRouter basename="site">
+      <HashRouter>
         <Header />
         <Routes>
           <Route path="/" element={<SearchVacancyPage />} />
@@ -17,7 +17,7 @@ function App() {
           <Route path="/favourites" element={<FavoriteVacancyPage />} />
           <Route path={"empty" && "*"} element={<EmptyPage />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
