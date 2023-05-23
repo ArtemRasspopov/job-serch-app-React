9f833de2576ef40e68876c87e4459097f5778613
ARTEM_JAN <Artem199833@gmail.com>
2023-05-23 15:26:53 +0300

    fix : deploy fix

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Header } from "./Header/Header";
 import EmptyPage from "../pages/EmptyPage/EmptyPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import VacancyPage from "../pages/VacancyPage/VacancyPage";
+// import VacancyPage from "../pages/VacancyPage/VacancyPage";
 import SearchVacancyPage from "../pages/SearchVacancyPage/SearchVacancyPage";
 import FavoriteVacancyPage from "../pages/FavoritesVacancyPage/FavoritesVacancyPage";
 
@@ -13,7 +13,7 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<SearchVacancyPage />} />
-          <Route path="/vacancy/:id" element={<VacancyPage />} />
+          {/* <Route path="/vacancy/:id" element={<VacancyPage />} /> */}
           <Route path="/favourites" element={<FavoriteVacancyPage/>}/>
           <Route path={'empty' && '*'} element={<EmptyPage />} />
         </Routes>
