0921388e5c3aec2e8b9a579aa4e6bb52930d6b17
ARTEM_JAN <Artem199833@gmail.com>
2023-06-06 09:27:57 +0300

    remove Authorization token

--- src/store/сatalogues/catalogues.api.ts
+++ src/store/сatalogues/catalogues.api.ts
@@ -9,8 +9,8 @@ export const cataloguesApi = createApi({
       "x-secret-key": "GEU4nvd3rej*jeh.eqp",
       "x-Api-App-Id":
         "v3.r.137440105.ffdbab114f92b821eac4e21f485343924a773131.06c3bdbb8446aeb91c35b80c42ff69eb9c457948",
-      Authorization:
-        "Bearer v3.r.137539487.07026ed24c017e28c9eb286b79f7d50fcb71a6b2.95817820a8b87b681017db06b1334f5c2d401ac3",
+      // Authorization:
+      //   "Bearer v3.r.137539487.07026ed24c017e28c9eb286b79f7d50fcb71a6b2.95817820a8b87b681017db06b1334f5c2d401ac3",
     },
   }),
   refetchOnFocus: true,
